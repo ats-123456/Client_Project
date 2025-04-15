e521a6be35d6319b6b010725d6fa42aab72fd0f4
ats-123456 <132741377+suresh-27-09@users.noreply.github.com>
2025-04-15 17:06:43 +0530

    whitenoise

--- attendance_system/wsgi.py
+++ attendance_system/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance_system.settings')
 
 application = get_wsgi_application()
+from whitenoise import WhiteNoise
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
