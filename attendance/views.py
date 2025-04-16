b8a4f922b92307cedd32a8df3b6764d9cba96783
ats-123456 <132741377+suresh-27-09@users.noreply.github.com>
2025-04-16 17:22:42 +0530

    staff

--- attendance/views.py
+++ attendance/views.py
@@ -22,15 +22,18 @@ def register_user(request):
 
 
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.exceptions import AuthenticationFailed
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class StaffTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        if not self.user.is_staff:
+            raise AuthenticationFailed("You are not authorized to login. Contact Admin.")
+
+        return data
+
+class StaffTokenObtainPairView(TokenObtainPairView):
+    serializer_class = StaffTokenObtainPairSerializer
